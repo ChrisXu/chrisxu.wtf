52f2159e67c1c1d8014fcda6134d4df69b538f75
Chris Xu <taterctl@gmail.com>
2019-07-14 11:18:15 +0200

    remove warnings

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Card from '../components/card'
 
 class AboutPage extends React.Component {
   render() {
@@ -17,7 +16,7 @@ class AboutPage extends React.Component {
             <p>Hi there,</p>
             <p>
               How is your day? What kind of curiosity brings you here? Anyway, nice to meet you.
-              I'm Chris from Taiwan🇹🇼, living in Amsterdam, unavoidably being a cyclist for a few years. 
+              I'm Chris from Taiwan<span role="img" aria-label="taiwan">🇹🇼</span>, living in Amsterdam, unavoidably being a cyclist for a few years. 
               I build apps, teach people code, and read books. 
             </p>
           </section>
