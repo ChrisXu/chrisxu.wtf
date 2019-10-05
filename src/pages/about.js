976e338b86ee2af9ecdf5e8bac6fb559bdd690e3
Chris Xu <taterctl@gmail.com>
2019-10-05 22:31:31 +0200

    Add upcoming show-up

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
-import Divider from '@material-ui/core/Divider';
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -18,14 +17,22 @@ class AboutPage extends React.Component {
         </aside> 
         <main>
           <section>
-            <p>Hi there,</p>
-            <p>I'm Chris from Taiwan, a software bartender in Amsterdam. I make mobile applications and development tools for living. I also like to build teams and engineer culture for fun.</p>
+            <h4>Hi there,</h4>
+            <p>I'm Chris from Taiwan<span role="img" aria-label="taiwan">🇹🇼</span>, a software engineer and Swift bartender in Amsterdam. I make mobile applications and development tools for living. I also like to build teams and engineer culture for fun.</p>
 
             <p>In the daytime, I enjoy reading books with classical music. At night, I turn into coding with electronic music. If you hear lo-fi music in my house, I am probably cooking.</p>
 
-            <p>Sometimes people find me boring because I tend to speak objectively, which is too plain to understand. I do have biases on many things, but I will try to eliminate those in my posts and talks. So please do feel free to hit me up, and also all kind of feedback is much appreciated.</p>
+            <p>Sometimes people find me boring because I tend to speak objectively, which is too plain to understand. I do have biases on many things, but I try to eliminate those in my posts and talks. So please do feel free to hit me up, and also all kind of feedback is much appreciated.</p>
           </section>
-          <Divider variant="middle" />
+          <hr></hr>
+          <section>
+            <h4>Upcoming show-up</h4>
+            <ul>
+              <li>Nov 15 - Nov 17 @ Glasgow, UK</li>
+              <li>Dec 19 - Dev 20 @ Tokyo, JP</li>
+              <li>Dec 21 - Jan 11 @ Everywhere, TW</li>
+            </ul>
+          </section>
         </main>
       </Layout>
     )
